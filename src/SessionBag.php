016d46e7f971863b28dbd2e44f5930029bce19ae
Andrew Stowell <andrew.stowell@byoplayground.com>
2020-02-24 18:10:02 +0000

    Rewritten, added logging support, resolved honeypot false-positive.

--- src/SessionBag.php
+++ src/SessionBag.php
@@ -3,18 +3,22 @@
 namespace VigilantForm\Kit;
 
 /**
- * Very simple session wrapper, to allow VigilantFormKit use either
- * Laravel Session or PHP Session with same interface.
+ * Very simple session wrapper, to allow VigilantFormKit to use either
+ * a Laravel Session or PHP Session with same interface.
  *
- * Will only open a session if one isn't already started.
+ * Will only open a session if it is not already started.
+ * Will only close the session if this class opened the session.
  *
- * Note: once save() is called, the session is closed and can not be used.
+ * Note: once save() is called, the session is closed and can not be used again.
  */
 class SessionBag
 {
     /** @var bool true if we should close the session when done */
     protected $close;
 
+    /**
+     * Will open a session if it is not already started.
+     */
     public function __construct()
     {
         $this->close = false;
@@ -25,6 +29,9 @@ class SessionBag
         }
     }
 
+    /**
+     * Will close the session, but only if we opened it.
+     */
     public function __destruct()
     {
         if ($this->close) {
@@ -32,22 +39,44 @@ class SessionBag
         }
     }
 
-    public function save(): void
+    /**
+     * Will close the session, but only if we opened it.
+     * @return bool Returns true on success, otherwise false.
+     */
+    public function save(): bool
     {
-        session_write_close();
-        $this->close = false;
+        if ($this->close) {
+            $this->close = false;
+            return session_write_close();
+        }
+        return false;
     }
 
+    /**
+     * A field which exists within the session may be set to null.
+     * @param string $key The name of the field within the session we are checking if exists.
+     * @return bool Returns true if field is defined within the session, otherwise false.
+     */
     public function exists(string $key): bool
     {
         return array_key_exists($key, $_SESSION);
     }
 
+    /**
+     * A field which exists within the session may be set to null; $default defaults to null.
+     * @param string $key The name of the field within the session we want to get.
+     * @param mixed $default Optional, what value to return should the session not have the field specified.
+     * @return mixed Returns the value of the fields within the session specified, otherwise the given default.
+     */
     public function get(string $key, $default = null)
     {
         return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
     }
 
+    /**
+     * @param string $key The name of the field within the session we want to store.
+     * @param mixed $value The value to store within this session field.
+     */
     public function put(string $key, $value = null): void
     {
         $_SESSION[$key] = $value;
