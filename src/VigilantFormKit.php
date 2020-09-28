31471f48f24e112be3dc550937a8f32c94fcf53e
Andrew Stowell <andrew@digitaladapt.com>
2020-09-28 17:06:37 -0400

    Javascript refined to handle all forms within the html that hasn't been handled yet.

--- src/VigilantFormKit.php
+++ src/VigilantFormKit.php
@@ -310,26 +310,31 @@ HTML;
      */
     public function generateScript(): string
     {
-        return <<<JAVASCRIPT
+        return <<<JS
 (function () {
-    var foo = document.getElementsByClassName("{$this->script_class}")[0];
-    if (foo) {
-        foo.style.position   = "absolute";
-        foo.style.height     = "11px";
-        foo.style.width      = "11px";
-        foo.style.textIndent = "11px";
-        foo.style.overflow   = "hidden";
-        foo.className        = "";
+    const joe = document.getElementsByClassName("{$this->script_class}");
+    if (joe) {
+        let foo, bar;
+        for (let i = joe.length - 1; i >= 0; --i) {
+            foo = joe[i];
 
-        var bar = foo.getElementsByTagName("input")[0];
-        if (bar) {
-            bar.tabIndex  = -1;
-            bar.value     = parseInt(foo.getAttribute("data-first")) + parseInt(foo.getAttribute("data-second"));
-            bar.className = "";
+            foo.style.position   = "absolute";
+            foo.style.height     = "11px";
+            foo.style.width      = "11px";
+            foo.style.textIndent = "11px";
+            foo.style.overflow   = "hidden";
+            foo.className        = "";
+
+            bar = foo.getElementsByTagName("input")[0];
+            if (bar) {
+                bar.tabIndex  = -1;
+                bar.value     = parseInt(foo.getAttribute("data-first")) + parseInt(foo.getAttribute("data-second")).toString();
+                bar.className = "";
+            }
         }
     }
 })();
-JAVASCRIPT;
+JS;
     }
 
     /**
--- vf-pn.js
+++ vf-pn.js
@@ -1,18 +1,23 @@
 (function () {
-    var foo = document.getElementsByClassName("vf-pn")[0];
-    if (foo) {
-        foo.style.position   = "absolute";
-        foo.style.height     = "11px";
-        foo.style.width      = "11px";
-        foo.style.textIndent = "11px";
-        foo.style.overflow   = "hidden";
-        foo.className        = "";
+    const joe = document.getElementsByClassName("vf-pn");
+    if (joe) {
+        let foo, bar;
+        for (let i = joe.length - 1; i >= 0; --i) {
+            foo = joe[i];
 
-        var bar = foo.getElementsByTagName("input")[0];
-        if (bar) {
-            bar.tabIndex  = -1;
-            bar.value     = parseInt(foo.getAttribute("data-first")) + parseInt(foo.getAttribute("data-second"));
-            bar.className = "";
+            foo.style.position   = "absolute";
+            foo.style.height     = "11px";
+            foo.style.width      = "11px";
+            foo.style.textIndent = "11px";
+            foo.style.overflow   = "hidden";
+            foo.className        = "";
+
+            bar = foo.getElementsByTagName("input")[0];
+            if (bar) {
+                bar.tabIndex  = -1;
+                bar.value     = parseInt(foo.getAttribute("data-first")) + parseInt(foo.getAttribute("data-second")).toString();
+                bar.className = "";
+            }
         }
     }
 })();
